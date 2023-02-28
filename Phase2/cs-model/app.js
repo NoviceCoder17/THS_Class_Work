dbcc2584cbede5e717d203bbe13fbe86cf30f08e
NoviceCoder17 <prema.pisces@gmail.com>
2023-02-27 22:36:08 -0500

    css

--- Phase2/cs-model/app.js
+++ Phase2/cs-model/app.js
@@ -39,29 +39,59 @@
 
 
 import express from 'express'
+import axios from 'axios'
 const app = express()
 const port = 5000
 
-app.use(express.json())
+app.use(express.json()) // middleware
 
-//This is to set endpoints
-app.get('/', (req, res) => {
-res.send('Hello World!')
-//   // //You cannot response twice
- // GET does not have body
-  console.log(req.body)
-  console.log(req.headers)
-  console.log(req.method)
+// //This is to set endpoints
+// app.get('/', (req, res) => {
+// res.send('Hello World!')
+// //   // //You cannot response twice
+//  // GET does not have body
+//   console.log(req.body)
+//   console.log(req.headers)
+//   console.log(req.method)
+// })
+
+// app.post('/',(req,res)=>{
+//   console.log(req.body)
+//   // console.log("Hello post")
+// })
+
+const cb1 = (req,res,next)=>{
+ 
+  //console.log("whatsup")
+  req.a = 34
+  next() //transfer control to next middleware
+ 
+}
+//Req,res,next= middleware function
+app.get("/",cb1,(req,res)=>{
+  console.log("hello")
+  console.log(req.a)
 })
 
-app.post('/',(res,req)=>{
-  console.log(res.body)
-  console.log("Hello post")
-})
-
+// app.get("/query",(req,res)=>{
+//   console.log(req.query)
+// })
 //Request can be made using 1. browser ip:port 2. From test.js using axios 3. Postman
 //Browsers can only make get request
 
+// To get specific userid we can use path variables
+// app.get("/:user_id",async(req,res)=>{
+//   try{
+//     let result = await axios.get(`https://apid.github.com/users/${req.params.user_id}`)
+//    // console.log(result.data)
+//     res.status(200).json({"success":result.data})  // shown in postman
+
+//   }
+//   catch(error){
+//     console.log(error)
+//     res.status(500).json({error :"system error"})  // shown in postman
+//   }
+// })
 app.listen(port, () => {
   console.log(`Server has started at  ${port}`)
 })
